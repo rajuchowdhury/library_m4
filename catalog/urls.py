5e3f5869173a941065db12355d2aa191fa10bfd1
raju <raju.fci58@gmail.com>
2018-01-10 20:56:50 +0600

    Initial commit

---
+++ catalog/urls.py
@@ -0,0 +1,20 @@
+# Use include() to add paths from the catalog application
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('/', RedirectView.as_view(url='/catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+
+urlpatterns = [
+
+]
