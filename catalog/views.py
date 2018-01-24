cdfa99f2e46132f572efd844d051853fb716ad05
raju <raju.fci58@gmail.com>
2018-01-24 19:57:13 +0600

    updated

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     """
@@ -14,15 +15,32 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count() # The 'all()' is implied by default.
     # Render the HTML template index.html with the data in the context variable
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     return render(
     request,
     'index.html',
-    context={'num_books': num_books, 'num_instances': num_instances,
-    'num_instances_available': num_instances_available, 'num_authors':
-    num_authors},
+    context={'num_books': num_books, 'num_instances': num_instances,'num_instances_available': num_instances_available, 'num_authors':num_authors,
+             'num_visits':num_visits},
 )
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 1
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+
+
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
